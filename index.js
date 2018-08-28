1fc078201c914e1cfaf78e3a940d6a99961e5bee
Sam Brozinsky <s77broz@aol.com>
2018-08-28 07:29:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(people, instrum) {
+  const arr = [];
+  for (let i = 0; i < people.length; i++) {
+    arr.push(`${people[i]} plays the ${instrum[i]}`)
+  }
+}
