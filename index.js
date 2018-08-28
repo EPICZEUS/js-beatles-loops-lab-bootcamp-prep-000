11851045d9952ead0ba80c91d7931a7c494414b8
Sam Brozinsky <s77broz@aol.com>
2018-08-28 07:29:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function theBeatlesPlay(people, instrum) {
   const arr = [];
   for (let i = 0; i < people.length; i++) {
-    arr.push(`${people[i]} plays the ${instrum[i]}`)
+    arr.push(`${people[i]} plays the ${instrum[i]}`);
   }
+  return arrl
 }
